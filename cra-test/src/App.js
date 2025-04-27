370111091c4bf3eae6a5a6a7ba680968c582e7f0
seongbae <pseongbae@gmail.com>
2025-04-27 18:07:36 +0900

    Feat: useEffect

--- cra-test/src/App.js
+++ cra-test/src/App.js
@@ -1,9 +1,22 @@
 import Button from "./Button";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => {
+    setCounter((prev) => prev + 1);
+  };
+  console.log("I run all the time");
+
+  const runOnlyOnce = () => {
+    console.log("I run only once");
+  };
+
+  useEffect(runOnlyOnce, []);
   return (
     <div>
-      <h1>Init test project</h1>
+      <h1>Effects: {counter}</h1>
+      <button onClick={onClick}>Click me</button>
       <Button text={"Hello"} />
     </div>
   );
