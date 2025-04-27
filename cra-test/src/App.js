329de9b168c0d71e729863c724f00056f09bd020
seongbae <pseongbae@gmail.com>
2025-04-27 18:35:21 +0900

    Study Deps

--- cra-test/src/App.js
+++ cra-test/src/App.js
@@ -3,18 +3,33 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
   const onClick = () => {
     setCounter((prev) => prev + 1);
   };
   console.log("I run all the time");
-
   const runOnlyOnce = () => {
     console.log("I run only once");
   };
 
   useEffect(runOnlyOnce, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes, ", keyword);
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes, ", counter);
+  }, [counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search"
+      />
       <h1>Effects: {counter}</h1>
       <button onClick={onClick}>Click me</button>
       <Button text={"Hello"} />
