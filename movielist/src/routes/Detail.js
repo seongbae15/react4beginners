a2d4e1ea9d605a3310952c03d6507bdc257ebe57
seongbae <pseongbae@gmail.com>
2025-04-30 19:42:04 +0900

    Feat: Parameters

--- movielist/src/routes/Detail.js
+++ movielist/src/routes/Detail.js
@@ -1,4 +1,18 @@
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+
 function Detail() {
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getMovie = async () => {
+      const json = await (
+        await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+      ).json();
+      console.log(json);
+    };
+    getMovie();
+  }, [id]);
   return <h1>Detail</h1>;
 }
 
